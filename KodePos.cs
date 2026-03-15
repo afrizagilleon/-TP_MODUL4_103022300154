f6cfb8168ca46abb8d46ea7854558f1e9b66edd0
Afriza Gilleon Ginting <41586762+afrizagilleon@users.noreply.github.com>
2026-03-16 01:33:57 +0700

    second commit: KodePos

---
+++ KodePos.cs
@@ -0,0 +1,28 @@
+﻿namespace TP_MODUL4_103022300154
+{
+    internal class KodePos
+    {
+        private Dictionary<string, string> dataKodePos = new Dictionary<string, string>
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari",  "40287"},
+            {"Mengger",     "40267"},
+            {"Wates",       "40256"},
+            {"Cijaura",     "40287"},
+            {"Jatisari",    "40286"},
+            {"Margasari",   "40286"},
+            {"Sekejati",    "40286"},
+            {"Kebonwaru",   "40272"},
+            {"Maleer",      "40274"}
+        };
+
+        public string getKodePos(string kelurahan)
+        {
+            if (dataKodePos.TryGetValue(kelurahan, out string kode))
+            {
+                return kode;
+            }
+            return "Kode pos tidak ditemukan";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL4_103022300154;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // TABLE DRIVEN: KodePos
+        KodePos kp = new KodePos();
+        Console.Write("Masukkan nama kelurahan: ");
+        string kel = Console.ReadLine();
+        string kode = kp.getKodePos(kel);
+        Console.WriteLine($"Kode pos {kel} adalah: {kode}");
+
+        
+    }
+}
