e7423c52ebb550d578c3dea9ae5ea1c26279a0c6
Afriza Gilleon Ginting <41586762+afrizagilleon@users.noreply.github.com>
2026-03-16 01:38:01 +0700

    add class DooMachine

---
+++ DoorMachine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022300154
+{
+    internal class DoorMachine
+    {
+        private enum State
+        {
+            Terkunci,
+            Terbuka
+        }
+
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void KunciPintu()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            currentState = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,24 @@ class Program
     static void Main(string[] args)
     {
         // TABLE DRIVEN: KodePos
-        KodePos kp = new KodePos();
-        Console.Write("Masukkan nama kelurahan: ");
-        string kel = Console.ReadLine();
-        string kode = kp.getKodePos(kel);
-        Console.WriteLine($"Kode pos {kel} adalah: {kode}");
+        //KodePos kp = new KodePos();
+        //Console.Write("Masukkan nama kelurahan: ");
+        //string kel = Console.ReadLine();
+        //string kode = kp.getKodePos(kel);
+        //Console.WriteLine($"Kode pos {kel} adalah: {kode}");
 
-        
+
+        // STATE-BASED: DoorMachine
+        DoorMachine door = new DoorMachine(); // awal: terkunci
+        Console.WriteLine("Tekan ENTER untuk membuka pintu...");
+        Console.ReadLine();
+        door.BukaPintu();
+
+        Console.WriteLine("Tekan ENTER untuk mengunci pintu lagi...");
+        Console.ReadLine();
+        door.KunciPintu();
+
+        Console.WriteLine("Tekan ENTER untuk keluar...");
+        Console.ReadLine();
     }
 }
